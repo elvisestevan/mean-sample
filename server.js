24d1c7c5d89363dbab18431f517cad0970107c16
Elvis Estevan <elvis.estevan65@gmail.com>
2016-06-23 21:05:10 +0000

    Parte 13-1 concluida

--- server.js
+++ server.js
@@ -24,11 +24,38 @@ app.set('views', path.resolve(__dirname, 'client', 'views'));
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
+var users = [];
 io.on('connection', function(socket) {
-  console.log('A user has connected!');
-  socket.on('disconnect', function() {
-    console.log('A user has disconnected');
+  var username = '';
+  console.log("A User has Connected!");
+  
+  socket.on('request-users', function(){
+    socket.emit('users', {users: users});
   });
+  
+  socket.on('message', function(data){
+    io.emit('message', {username: username, message: data.message});
+  })
+  
+  socket.on('add-user', function(data){
+    if(users.indexOf(data.username) == -1){
+      io.emit('add-user', {
+        username: data.username
+      });
+      username = data.username;
+      users.push(data.username);
+    } else {
+      socket.emit('prompt-username', {
+        message: 'User Already Exists'
+      })
+    }
+  })
+  
+  socket.on('disconnect', function(){
+    console.log(username + ' has disconnected!');
+    users.splice(users.indexOf(username), 1);
+    io.emit('remove-user', {username: username});
+  })
 });
 
 app.get('/', function (req, res) {
